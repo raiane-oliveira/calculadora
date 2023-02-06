be5a6989924be1970e175a1e279456250eaed048
Raiane <raiane.oliveira404@gmail.com>
2023-02-06 08:17:29 -0300

    Estilos concertados e expressões sendo mostradas na tela

--- index.js
+++ index.js
@@ -2,37 +2,59 @@ let displayCalculator = document.querySelector(".display-calculator");
 let operator = "";
 let number1 = "",
     number2 = "";
+let expression = "";
 
 document.addEventListener("click", (event) => {
     let element = event.target;
 
     if (element.dataset.clear === "c") {
         displayCalculator.innerHTML = "";
-        number1 = "";
-        number2 = "";
-        operator = "";
+        clearCalculator();
     }
 
     if (element.dataset.operator) {
         operator = element.dataset.operator;
-        console.log(operator);
     }
 
     if (element.dataset.number) {
         if (!operator) {
-            number1 += element.dataset.number;
+            // Substitui 0 pelo número digitado
+            number1 =
+                number1 == 0
+                    ? element.dataset.number
+                    : number1 + element.dataset.number;
             console.log(number1);
         } else {
-            number2 += element.dataset.number;
+            number2 =
+                number2 == 0
+                    ? element.dataset.number
+                    : number2 + element.dataset.number;
             console.log(number2);
         }
     }
 
+    // Troca o sinal do número
+    if (element.dataset.sign) {
+        if (!operator) {
+            number1 *= -1;
+        } else {
+            number2 *= -1;
+        }
+    }
+
     let result = calculate(Number(number1), operator, Number(number2));
-    console.log(result);
+    expression = `${number1} ${operator} ${number2}`;
+    console.log(expression);
+    showOnScreen(expression);
 
     if (element.dataset.equal) {
-        displayCalculator.innerHTML = result;
+        displayCalculator.innerHTML = `
+        <p class="oldExpression">${expression}</p>
+        <img class="equal-expression" src="assets/Equals.svg" alt="Equal">
+        <span>${result}</span>`;
+
+        // Limpa todos os campos
+        clearCalculator();
     }
 });
 
@@ -45,7 +67,7 @@ function calculate(firstNumber, operator, secondNumber) {
         case "-":
             result = firstNumber - secondNumber;
             break;
-        case "*":
+        case "x":
             result = firstNumber * secondNumber;
             break;
         case "/":
@@ -54,13 +76,19 @@ function calculate(firstNumber, operator, secondNumber) {
         case "%":
             result = (firstNumber * secondNumber) / 100;
             break;
+        default:
+            result = firstNumber ? firstNumber : secondNumber;
+            break;
     }
     return result;
 }
 
 function showOnScreen(text) {
-    let spanText = document.createElement("span");
-    spanText.innerHTML = text;
+    displayCalculator.innerHTML = `<span>${text}</span>`;
+}
 
-    displayCalculator.append(spanText);
+function clearCalculator() {
+    number1 = "";
+    number2 = "";
+    operator = "";
 }
